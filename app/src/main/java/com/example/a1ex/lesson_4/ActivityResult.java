2480cc4af0a87067940acff90f4f93af91155879
A1ex2 <32175773+A1ex2@users.noreply.github.com>
2018-01-26 08:49:02 +0200

    26.01.2018

--- app/src/main/java/com/example/a1ex/lesson_4/ActivityResult.java
+++ app/src/main/java/com/example/a1ex/lesson_4/ActivityResult.java
@@ -1,13 +1,24 @@
 package com.example.a1ex.lesson_4;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.a1ex.objects.LoginInfo;
 
 public class ActivityResult extends AppCompatActivity {
+    private TextView txtResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent = getIntent();
+        LoginInfo loginInfo = (LoginInfo)intent.getSerializableExtra(MainActivity.LOGIN_INFO);
+
+        txtResult = (TextView) findViewById(R.id.txtResult);
+        txtResult.setText(String.format(txtResult.getText().toString(), loginInfo.getLogin()));
     }
 }
