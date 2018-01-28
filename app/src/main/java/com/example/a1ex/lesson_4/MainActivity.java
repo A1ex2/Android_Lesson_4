64ab28b08ff9d1b592dfb68e0eb2b3dbf1b5f03f
A1ex2 <32175773+A1ex2@users.noreply.github.com>
2018-01-28 18:45:17 +0200

    28.01.2018

--- app/src/main/java/com/example/a1ex/lesson_4/MainActivity.java
+++ app/src/main/java/com/example/a1ex/lesson_4/MainActivity.java
@@ -10,17 +10,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.view.Gravity;
 import android.widget.Button;
 import android.view.View;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a1ex.objects.LoginInfo;
 
 import java.util.DuplicateFormatFlagsException;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements OnClickListener{
+//public class MainActivity extends AppCompatActivity implements OnClickListener{
+public class MainActivity extends AppCompatActivity{
     public static final String LOGIN_INFO = "com.example.a1ex.loginInfo";
 
     public TextView txtLogin;
@@ -44,12 +47,12 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         prepareDialogBuilder();
     }
 
-    private void shovUserDialog(int messageId){
-        dlgBuilder.setMessage(messageId);
-        AlertDialog alertDialog = dlgBuilder.create();
-        alertDialog.show();
-
-    }
+//    private void showUserDialog(int messageId){
+//        dlgBuilder.setMessage(messageId);
+//        AlertDialog alertDialog = dlgBuilder.create();
+//        alertDialog.show();
+//
+//    }
 
     private void defineComponents(){
         txtLogin = (TextView) findViewById(R.id.txtLogin);
@@ -76,12 +79,15 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
 
             if (loginInfo.getLogin()== null
                     || loginInfo.getLogin().trim().equals("")){
-                shovUserDialog(R.string.login_required);
+//                showUserDialog(R.string.login_required);
+                    showToastMessage(R.string.login_required);
                     txtLogin.requestFocus();
                     return;
             } else if (loginInfo.getPassword()== null
                     || loginInfo.getPassword().trim().equals("")){
-                shovUserDialog(R.string.password_required);
+//                showUserDialog(R.string.password_required);
+                showToastMessage(R.string.password_required);
+                txtLogin.requestFocus();
                 txtPassword.requestFocus();
                 return;
             }
@@ -97,12 +103,18 @@ public class MainActivity extends AppCompatActivity implements OnClickListener{
         }
     }
 
-    @Override
-    public void onClick(DialogInterface dialogInterface, int i) {
-        if (i == dialogInterface.BUTTON_POSITIVE){
-            System.out.println("OK");
-        }else if (i == dialogInterface.BUTTON_NEGATIVE){
-            System.out.println("Cancel");
-        }
+    private void showToastMessage(int messageId){
+        Toast message = Toast.makeText(this, messageId, Toast.LENGTH_LONG);
+        message.setGravity(Gravity.TOP, 0, 100);
+        message.show();
     }
+
+//    @Override
+//    public void onClick(DialogInterface dialogInterface, int i) {
+//        if (i == dialogInterface.BUTTON_POSITIVE){
+//            System.out.println("OK");
+//        }else if (i == dialogInterface.BUTTON_NEGATIVE){
+//            System.out.println("Cancel");
+//        }
+//    }
 }
