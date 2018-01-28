fde11207d5393e9565d6ad0a2fa7cb2ad65580c6
A1ex2 <32175773+A1ex2@users.noreply.github.com>
2018-01-28 18:32:26 +0200

    28.01.2018

--- app/src/main/java/com/example/a1ex/lesson_4/MainActivity.java
+++ app/src/main/java/com/example/a1ex/lesson_4/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.a1ex.lesson_4;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.widget.Button;
+import android.view.View;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.a1ex.objects.LoginInfo;
 
 import java.util.DuplicateFormatFlagsException;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
     public static final String LOGIN_INFO = "com.example.a1ex.loginInfo";
 
     public TextView txtLogin;
@@ -20,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
     public Button btnLogin;
     public Button btnClear;
 
+    private Spinner spinnerDevices;
+    private AlertDialog.Builder dlgBuilder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,24 +39,70 @@ public class MainActivity extends AppCompatActivity {
 
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnClear = (Button) findViewById(R.id.btnClear);
-        txtPassword = (TextView) findViewById(R.id.txtPassword);
-        txtLogin = (TextView) findViewById(R.id.txtLogin);
+
+        defineComponents();
+        prepareDialogBuilder();
     }
+
+    private void shovUserDialog(int messageId){
+        dlgBuilder.setMessage(messageId);
+        AlertDialog alertDialog = dlgBuilder.create();
+        alertDialog.show();
+
+    }
+
+    private void defineComponents(){
+        txtLogin = (TextView) findViewById(R.id.txtLogin);
+        txtPassword = (TextView) findViewById(R.id.txtPassword);
+
+        spinnerDevices = (Spinner) findViewById(R.id.spinnerDevices);
+
+        txtLogin.requestFocus();
+    }
+
+    private void prepareDialogBuilder(){
+        dlgBuilder = new AlertDialog.Builder(this);
+        dlgBuilder.setTitle(R.string.error);
+//        dlgBuilder.setNegativeButton("Cancel", (DialogInterface.OnClickListener) this);
+        dlgBuilder.setPositiveButton("OK", (DialogInterface.OnClickListener) null);
+    }
+
     public void buttonClick(View view){
-        if (view == btnLogin){
+        if (view.getId() == R.id.btnLogin){
 
             LoginInfo loginInfo = new LoginInfo();
             loginInfo.setLogin(txtLogin.getText().toString());
             loginInfo.setPassword(txtPassword.getText().toString());
 
+            if (loginInfo.getLogin()== null
+                    || loginInfo.getLogin().trim().equals("")){
+                shovUserDialog(R.string.login_required);
+                    txtLogin.requestFocus();
+                    return;
+            } else if (loginInfo.getPassword()== null
+                    || loginInfo.getPassword().trim().equals("")){
+                shovUserDialog(R.string.password_required);
+                txtPassword.requestFocus();
+                return;
+            }
+
             Intent resultIntent = new Intent(this, ActivityResult.class);
             resultIntent.putExtra(LOGIN_INFO, loginInfo);
             startActivity(resultIntent);
 
-        }  else if (view == btnClear){
+        }  else if (view.getId() == R.id.btnClear){
             txtLogin.setText("");
             txtPassword.setText("");
             txtLogin.requestFocus();
+        }
+    }
+
+    @Override
+    public void onClick(DialogInterface dialogInterface, int i) {
+        if (i == dialogInterface.BUTTON_POSITIVE){
+            System.out.println("OK");
+        }else if (i == dialogInterface.BUTTON_NEGATIVE){
+            System.out.println("Cancel");
         }
     }
 }
